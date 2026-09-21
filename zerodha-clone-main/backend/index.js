4ce4e66ee697a764a96ff35ed528963325545b97
Rohan Yadav <136440279+Rohanyadav24@users.noreply.github.com>
2026-09-22 01:49:16 +0530

    Update index.js

--- zerodha-clone-main/backend/index.js
+++ zerodha-clone-main/backend/index.js
@@ -9,6 +9,7 @@ const cors = require("cors");
 const { HoldingsModel } = require("./model/HoldingsModel"); 
 
 
+
 const { PositionsModel } = require("./model/PositionsModel");  
 const { OrdersModel } = require("./model/OrdersModel");
 
